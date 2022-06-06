f9452786bae1115e244632b75e33b739ce045197
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-06 11:56:18 -0300

    [Task: Navbar bugs]{Jun.06.2022}

---
+++ Frontend/blog-pessoal/src/componentes/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import './Navbar.css';
+import useLocalStorage from "react-use-localstorage";
+
+function Navbar() {
+
+    const [token, setToken] = useLocalStorage('token');
+    const [id, setId] = useLocalStorage('id');
+
+    let navigate = useNavigate();
+
+    function goLogout() {
+        setToken('')
+        setId('')
+        alert("Usuário deslogado")
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography className="titulo" variant="h5" color="inherit">
+                            Blog Pessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Box mx={1} className='cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                Logout
+                            </Typography>
+                        </Box>
+
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
