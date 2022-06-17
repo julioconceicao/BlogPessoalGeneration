725e183c88123886addc21a5f1c4df011f515ecc
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-17 01:08:07 -0300

    [Pagina login update ]{Jun.17.2022}

--- Frontend/blog-pessoal/src/paginas/login/Login.tsx
+++ Frontend/blog-pessoal/src/paginas/login/Login.tsx
@@ -6,11 +6,16 @@ import useLocalStorage from "react-use-localstorage";
 import { login } from "../../servicos/Servicos";
 import Usuario from "../../modelos/Usuario";
 import './Login.css';
+import { useDispatch } from "react-redux";
+import { addToken } from "../../store/tokens/actions";
+import { toast } from "react-toastify";
 
 function Login() {
 
     let navigate = useNavigate();
-    const [token, setToken] = useLocalStorage('token');
+    const dispatch = useDispatch();
+    const [token, setToken] = useState('');
+    const [idCriador, setIdCriador] = useLocalStorage('id');
 
     const [usuario, setUsuario] = useState<Usuario>(
         {
@@ -23,7 +28,7 @@ function Login() {
         }
     );
 
-    function updatedModel(e: ChangeEvent<HTMLInputElement>){
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
 
         setUsuario({
             ...usuario,
@@ -31,64 +36,84 @@ function Login() {
         })
     }
 
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
-        
-        try {
-            await login(`/api/Usuarios/logar`, usuario, setToken);
 
-            alert('Usuário logado com sucesso!');    
+        try {
+            await login(`/api/Usuarios/logar`, usuario, setToken, setIdCriador);
+
+            toast.success('Usuário logado com sucesso!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: 'colored',
+                progress: undefined
+            });
         } catch (error) {
-            alert('Dados do usuário inconsistentes. Erro ao logar!');
+            toast.error('Dados inconsistentes. Favor verificar as informações de login!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: 'colored',
+                progress: undefined
+            });
         }
     }
 
     useEffect(() => {
 
-        if(token !== ''){
+        if (token !== '') {
+            dispatch(addToken(token))
             navigate('/home');
         }
 
     }, [token, navigate]);
 
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid alignItems='center' xs={6}>
-                <Box paddingX={20}>
-                    <form onSubmit={onSubmit}>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        
-                        <TextField
-                            value={usuario.email}
-                            onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                            id='email' label='email' variant='outlined' name='email' margin='normal' fullWidth/>
+        <>
+            <Grid container direction='row' justifyContent='center' alignItems='center'>
+                <Grid alignItems='center' xs={6}>
+                    <Box paddingX={20}>
+                        <form onSubmit={onSubmit}>
+                            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
 
-                        <TextField
-                            value={usuario.senha}
-                            onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                            id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                            <TextField
+                                value={usuario.email}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id='email' label='email' variant='outlined' name='email' margin='normal' fullWidth />
 
-                        <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary'>
-                                Logar
-                            </Button>
+                            <TextField
+                                value={usuario.senha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
+                            <Box marginTop={2} textAlign='center'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                            </Box>
+                        </form>
+                        <Box display='flex' justifyContent='center' marginTop={2}>
+                            <Box marginRight={1}>
+                                <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            </Box>
+                            <Link to='/cadastrousuario'>
+                                <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                            </Link>
                         </Box>
-                    </form>
-                    <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-                        </Box>
-                        <Link to='/cadastrousuario'>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
-                        </Link>
-                            
                     </Box>
-                </Box>
-            </Grid>
-            <Grid xs={6} className='imagem'>
+                </Grid>
+                <Grid xs={6} className='imagem'>
 
+                </Grid>
             </Grid>
-        </Grid>
+        </>
     );
 }
 
